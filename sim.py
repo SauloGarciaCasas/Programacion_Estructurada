f7f40c3cee891ac6fd287b026a3a171b4efeee16
SauloGarciaCasas <saulo_3141240224@utd.edu.mx>
2025-07-15 09:37:56 -0600

    Delete sim.py

--- sim.py
+++
@@ -1,8 +0,0 @@
-lista=[]
-
-if len(lista)==0:
-    resp = True
-    while resp == "si":
-        lista.append(input("Dame una palabra o frase: ")).upper()
-        resp = input("Deseas solicitar otra frase? Si/No").lower().strip()
-    print(lista)
